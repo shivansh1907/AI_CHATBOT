2b6f629e192a365d0554d5bbea843167118343a9
shivansh1907 <dinku.cool19@gmail.com>
2025-07-26 13:05:30 +0530

    designing side bar

--- chatgpt/src/App.jsx
+++ chatgpt/src/App.jsx
@@ -1,5 +1,10 @@
 import { useState } from 'react'
 import gptlogo from './assets/chatgpt.svg'
+import addBtn from './assets/add-30.png'
+import msgIcon from './assets/message.svg'
+import home from './assets/home.svg'
+import message from './assets/message.svg'
+import rocket from './assets/rocket.svg'
 
 
 function App() {
@@ -10,13 +15,15 @@ function App() {
     <div className="sidebar">
       <div className="upper-side">
         <div className="uppersidetop">
-          <img src={gptlogo} alt="" className='logo' /> <span className="brandName">chatGPT</span>
-          <button className="midBtn">NEW CHAT</button>
+          <img src={gptlogo} alt="" className='logo' />
+           <span className="brandName">chatGPT</span>
+          
         </div>
 
         <div className="uppersidebottom">
-          <button className="query">What is Programming</button>
-          <button className="query">What is Programming</button>
+          <button className="midBtn"><img src={addBtn} alt="" className='addBtn'/>NEW CHAT</button>
+          <button className="query"><img src={msgIcon} alt="" />What is Programming</button>
+          <button className="query"><img src={msgIcon} alt="" />How to use an API?</button>
 
         </div>
         
@@ -25,6 +32,9 @@ function App() {
       </div>
 
       <div className="lower-side">
+        <div className="listItems"><img src={home} alt="" className="listitemsimg" />Home</div>
+         <div className="listItems"><img src={message} alt="" className="listitemsimg" />Save</div>
+          <div className="listItems"><img src={rocket} alt="" className="listitemsimg" />Upgrade</div>
 
       </div>
     </div>
